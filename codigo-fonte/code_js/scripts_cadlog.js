96a8af7dac889bda78fa43ee7b913546946132fc
Rafael <pessoa.carvalho@outlook.com>
2024-06-09 21:09:53 -0300

    profile_redirect

--- codigo-fonte/code_js/scripts_cadlog.js
+++ codigo-fonte/code_js/scripts_cadlog.js
@@ -78,7 +78,7 @@ function saveData2(){
 
         localStorage.setItem("name",current_user.name);
         localStorage.setItem("email",current_user.email);
-        // window.location.href="profile.html"
+        window.location.href="../code_html/html_perfil.html"
     }
     else {
         alert("Login Fail")
